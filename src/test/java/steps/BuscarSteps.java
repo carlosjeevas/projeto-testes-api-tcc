814de067233584cce54c223ca867c956e9e7c51a
Carlos Moreira <cmoreira.taking@veloe.com.br cmoreira.taking@veloe.com.br>
2022-03-13 00:40:54 -0300

    Criação dos métodos steps para a funcionalidade Buscar

--- src/test/java/steps/BuscarSteps.java
+++ src/test/java/steps/BuscarSteps.java
@@ -1,5 +1,33 @@
 package steps;
 
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+
 public class BuscarSteps {
+
+    @Dado( "que realizo a busca de usuario via api" )
+    public void que_realizo_a_busca_de_usuario_via_api() {
+
+    }
+
+    @Dado( "que realizo a busca de usuario pelo numero de identificacao {string}" )
+    public void que_realizo_a_busca_de_usuario_pelo_numero_de_identificacao( String id ) {
+
+    }
+
+    @Então( "valido a lista de usuarios retornada" )
+    public void valido_a_lista_de_usuarios() {
+
+    }
+
+    @Então( "valido o retorno da api com {string}" )
+    public void valido_o_retorno_da_api( String retorno ) {
+
+    }
+
+    @Então( "valido que o usuario foi retornado com sucesso" )
+    public void valido_que_o_usuario_foi_retornado_com_sucesso() {
+
+    }
 
 }
